ce34defced9d8498eb1307016d8d675bad70d296
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-05 14:52:02 -0600

    input header information and basic styling

---
+++ my-app/src/Components/Header/index.jsx
@@ -0,0 +1,10 @@
+import { HeaderContainer, HeaderName, Subheader } from "./styles";
+
+export const Header = () => {
+    return (
+        <HeaderContainer>
+            <HeaderName>Carly Staheli</HeaderName>
+            <Subheader>Personal Coding Portfolio</Subheader>
+        </HeaderContainer>
+    )
+}
