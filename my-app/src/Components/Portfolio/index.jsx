cc1c581c080f0bee5b4bab5e9f6b12300b12381e
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-05 15:06:39 -0600

    inported portfolio information

---
+++ my-app/src/Components/Portfolio/index.jsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { DeployedLinkButton, ProjectBox, ProjectContainer, ProjectName } from "./styles";
+
+
+export const PortfolioPage = ({ projects }) => {
+  console.log(projects)
+  return (
+    <ProjectContainer>
+      {projects.map((project) => (
+        <ProjectBox>
+          <ProjectName>{project.displayName}</ProjectName>
+          <DeployedLinkButton target = "_blank" href={project.repositoryLink}><FontAwesomeIcon size="2x" icon={faGithub} /></DeployedLinkButton>
+          <DeployedLinkButton target = "_blank" href={project.gitHubPagesLink}>View Page</DeployedLinkButton>
+        </ProjectBox>
+      ))}
+    </ProjectContainer>
+  );
+};
