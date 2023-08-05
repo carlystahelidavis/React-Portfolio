f11c92000669f939dcb9594d528f7307bd698952
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-05 14:48:15 -0600

    input footer information and basic styling

---
+++ my-app/src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import { FooterContainer, FooterName } from "./styles";
+
+export const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterName>Thanks for visiting my portfolio!</FooterName>
+        </FooterContainer>
+    )
+}
