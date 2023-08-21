dcb170c920b4290729d9f4d5f8769a53b682b80a
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-20 23:01:30 -0600

    Updates

--- src/Components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import { FooterContainer, FooterName } from "./style";
-
-export const Footer = () => {
-  return (
-    <FooterContainer>
-      <FooterName>Thanks for visiting my portfolio!</FooterName>
-    </FooterContainer>
-  );
-};
